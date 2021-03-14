50dc7f91cce36a1c13bc41460fd122cae05f792f
Bushra05Rafia <52478317+Bushra05Rafia@users.noreply.github.com>
2021-03-14 17:28:50 +0600

    updated

--- Cube/main.cpp
+++ Cube/main.cpp
@@ -1,30 +1,28 @@
 #include<windows.h>
 #include<GL/glut.h>
-float angle=0.0;
+float angle=0.0; //globally defined
 void spin(void)
 {
-    angle =angle +0.01;
+    angle =angle +0.001;
     glutPostRedisplay();
 }
 
 void display(void)
 {
     /* glColor3f(0,1,0);
-    glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
-    gluLookAt(0,0,4,0,0,0,0,1,0);
-    glRotatef(angle,1,0,0);
-    glRotatef(angle,0,1,0);
-    glRotatef(angle,0,0,1); */
+    gluLookAt(0,0,4,0,0,0,0,1,0); */
     // glutWireCube(1.0);// Built in function for drawing cube with size in 4 size
     //glutWireSphere(1,20,10); //
     // glutWireCone(1,2,20,10);//
     //glutWireTorus(0.5,1,20,40);//
-    // glutWireTeapot(1); //
-    // glutSolidCube(1); //
-    glRotatef(45,1,1,1); //45 degree angle rotate with respect to x,y,z
-    glutWireCube(1);
-    glutSolidCube(1);
+    glClear(GL_COLOR_BUFFER_BIT); //Clearing previous display
+    //glRotatef(angle,1,1,1); //45 degree angle rotate with respect to x,y,z
+    glRotatef(angle,1,0,0); // rotate with only x
+    glRotatef(angle,0,1,0);// rotate with only y
+    glRotatef(angle,0,0,1);// rotate with only z
+    glutWireCube(0.5);
+    //glutSolidCube(1);
     glFlush();
 }
 
@@ -43,7 +41,7 @@ int main(int argc, char **argv)
     glutInitWindowSize(500,500);
     glutInitWindowPosition(500,100);
     glutCreateWindow("Rotate Qurd Alone With The Circumference Of The Circle");
-    //glutIdleFunc(spin);
+    glutIdleFunc(spin);
     //glutReshapeFunc(reshape);
     glutDisplayFunc(display);
     glutMainLoop();
